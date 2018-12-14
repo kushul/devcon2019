5095956f0b6ca7dfd117fcf41defcd01a3dfec1b
Sandeep Ramgolam <sandeep@lsl.digital>
2018-12-14 15:29:47 +0400

    sponsors are now dynamic

--- web/app/src/components/homepage/sponsor-section.vue
+++ web/app/src/components/homepage/sponsor-section.vue
@@ -4,26 +4,31 @@
       <h2 class="mega-rainbow">Sponsors</h2>
       <h4 class="subtitle">Platinum</h4>
       <div class="sponsors-wrapper platinum-wrapper">
-        <a href="#"
-           class="sponsor-logo">
-          <img src="../../assets/sample-sponsors/mcb.png"
-               alt>
-        </a>
-        <a href="#"
-           class="sponsor-logo">
-          <img src="../../assets/sample-sponsors/sdworx.png"
-               alt>
+        <a
+          :href="sponsor.website"
+          class="sponsor-logo"
+          v-if="getSponsors && sponsor.status === 'Platinum'"
+          v-for="sponsor in getSponsors"
+          :key="sponsor.id"
+          :title="sponsor.name"
+        >
+          <img :src="sponsor.logo" :alt="sponsor.name" :title="sponsor.name">
         </a>
       </div>
 
-      <a href="#"
-         class="view-more">View all sponsors</a>
+      <a href="#" class="view-more">View all sponsors</a>
 
-      <div class="tribute-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea itaque magni, omnis cum reprehenderit possimus laudantium, doloribus veritatis dolorem repellat explicabo odio quas architecto nobis error nisi. Molestiae, sunt.</div>
+      <div
+        class="tribute-text"
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea itaque magni, omnis cum reprehenderit possimus laudantium, doloribus veritatis dolorem repellat explicabo odio quas architecto nobis error nisi. Molestiae, sunt.</div>
     </div>
 
     <div class="button-center">
-      <button-waw :theme="'#97d0e1'" :size="'80px'" :link="'https://github.com/mscraftsman/devcon2019/raw/master/assets/DevCon2019-SponsorshipProposal.pdf'">Become a Sponsor</button-waw>
+      <button-waw
+        :theme="'#97d0e1'"
+        :size="'80px'"
+        :link="'https://github.com/mscraftsman/devcon2019/raw/master/assets/DevCon2019-SponsorshipProposal.pdf'"
+      >Become a Sponsor</button-waw>
     </div>
   </div>
 </template>
@@ -33,6 +38,15 @@ import { mapGetters, mapActions } from "vuex";
 import { FETCH_SPONSORS } from "@/store";
 
 export default {
+  data() {
+    return {
+      sponsorsSample: [
+        { "id": "1", "name": "Jochen Kirstätter", "logo": "https://raw.githubusercontent.com/mscraftsman/devcon2019/dev/web/app/src/assets/sample-sponsors/mcb.svg", "website": "http://jochen.kirstaetter.name", "status": "Platinum" },
+        { "id": "2", "name": "Sandeep R", "logo": "https://raw.githubusercontent.com/mscraftsman/devcon2019/dev/web/app/src/assets/sample-sponsors/sdworx.svg", "website": "http://barfii.net", "status": "Gold" },
+        { "id": "3", "name": "Sandeep R", "logo": "https://raw.githubusercontent.com/mscraftsman/devcon2019/dev/web/app/src/assets/sample-sponsors/sdworx.svg", "website": "http://barfii.net", "status": "Gold" }
+      ]
+    }
+  },
   created() {
     this.FETCH_SPONSORS();
   },
